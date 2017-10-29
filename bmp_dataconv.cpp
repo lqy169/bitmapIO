cd9e68db4d63ead6c738ba4fdcf84147fa3f662b
peytonthehuman <33160477+peytonthehuman@users.noreply.github.com>
2017-10-29 15:37:57 -0500

    Created data converter functions
    Functions convert Pixel RGB data to a single data value in the data array. There are several different ways to do this.

---
+++ bmp_dataconv.cpp
@@ -0,0 +1,42 @@
+#include "iobmp.h"
+
+// Reverse Data Conversion ========================================================================
+
+void bmpFile::convertPixel2Data_BW() {
+	for(int i = 0; i < pixelArray.size(); i++) {
+		if(DEBUG_LEVEL >= 2) { std::cout << "#Converting pixel " << i << "::" << std::endl; }
+		int r = pixelArray[i].rgbRed;
+		int g = pixelArray[i].rgbGreen;
+		int b = pixelArray[i].rgbBlue;
+
+		double bw = (std::min(r, std::min(g, b)) + std::max(r, std::max(g, b))) * 0.5f;
+
+		pixelData.push_back(bw);
+	}
+	
+}
+
+void bmpFile::convertPixel2Data_AVG() {
+	for(int i = 0; i < pixelArray.size(); i++) {
+		if(DEBUG_LEVEL >= 2) { std::cout << "#Converting pixel " << i << "::" << std::endl; }
+		double avg = pixelArray[i].rgbRed + pixelArray[i].rgbGreen + pixelArray[i].rgbBlue;
+		avg /= 3;
+		pixelData.push_back(avg);
+	}
+}
+
+void bmpFile::convertPixel2Data_RGB() {
+	for(int i = 0; i < pixelArray.size(); i++) {
+		int testVal = (i + 1) % 3;
+		if(testVal == 2) {
+			pixelData.push_back(pixelArray[i].rgbRed);
+		}
+		if(testVal == 1) {
+			pixelData.push_back(pixelArray[i].rgbGreen);
+		}
+		if(testVal == 0) {
+			pixelData.push_back(pixelArray[i].rgbBlue);
+		}
+		if(DEBUG_LEVEL >= 2) { std::cout << "#Converting pixel " << i << "::" << std::endl; }
+	}
+}
